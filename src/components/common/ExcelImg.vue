63dcffb1c7e84d38d7fb703806c6a1696c65dc1c
lianzheng fenxian System <1304066854@qq.com>
2019-03-15 09:16:27 +0800

    ‘添加excel表格上传功能’

---
+++ src/components/common/ExcelImg.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="excel-wrap" ref="excel">
+    <img src="../../assets/img/excel.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExcelImg'
+}
+</script>
+
+<style scoped>
+.excel-wrap{
+
+}
+</style>
